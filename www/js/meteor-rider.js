bfd2ed7dc1d1b2e4802618cc509bf1b4ffedc16d
Jon Gaulin <gaulinj1985@hotmail.com>
2014-06-22 18:10:57 -0400

    Fixing mobile

--- www/js/meteor-rider.js
+++ www/js/meteor-rider.js
@@ -72,6 +72,7 @@ var MeteorRider = {
         //     all HTML is replaced/overwritten
         //     all new CSS/JS is loaded
           alert($('.logo').length)
+          $('.logo').hide()
           $('.logo').delay(3000).fadeOut(500, function(){
             alert('doc')
             // document.open();
